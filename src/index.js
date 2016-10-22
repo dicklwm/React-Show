dab27a982f923b4ed6ec7a605b35af9ea2e53590
Min <dicklwm@163.com>
2016-10-22 20:44:47 +0800

    React-Rate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Rate from './Rate/index.js';
+
+ReactDOM.render(
+  <Rate count="8" value="6"/>,
+  document.getElementById('root')
+);
